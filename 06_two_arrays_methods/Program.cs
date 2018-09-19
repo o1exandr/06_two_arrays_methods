41c312b8abef77b34ccf2194087cb8a5b296356d
olexandr.finyuk <olexandr.finyuk@gmail.com>
2018-09-19 20:46:04 +0300

    Add project files.

---
+++ 06_two_arrays_methods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_two_arrays_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
